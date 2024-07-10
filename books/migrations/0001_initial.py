11a02aa12b3f00f50ff0cbf9939256c273b7f494
Erik <erik.agayan1@gmail.com>
2024-07-10 14:19:03 +0300

    Used black on files

--- books/migrations/0001_initial.py
+++ books/migrations/0001_initial.py
@@ -7,22 +7,29 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Book',
+            name="Book",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=255)),
-                ('author', models.CharField(max_length=255)),
-                ('published_date', models.DateField()),
-                ('isbn', models.CharField(max_length=13)),
-                ('pages', models.IntegerField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=255)),
+                ("author", models.CharField(max_length=255)),
+                ("published_date", models.DateField()),
+                ("isbn", models.CharField(max_length=13)),
+                ("pages", models.IntegerField()),
             ],
             options={
-                'db_table': 'books',
+                "db_table": "books",
             },
         ),
     ]
--- books/tests.py
+++ books/tests.py
@@ -9,43 +9,43 @@ class BookTests(APITestCase):
 
     def setUp(self):
         self.valid_data = {
-            'title': 'Test Book',
-            'author': 'Author Name',
-            'published_date': '2023-01-01',
-            'isbn': '1234567890123',
-            'pages': 100
+            "title": "Test Book",
+            "author": "Author Name",
+            "published_date": "2023-01-01",
+            "isbn": "1234567890123",
+            "pages": 100,
         }
         self.book = Book.objects.create(**self.valid_data)
-        self.url = reverse('book-list')
+        self.url = reverse("book-list")
 
     def test_create_book(self):
-        response = self.client.post(self.url, self.valid_data, format='json')
+        response = self.client.post(self.url, self.valid_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Book.objects.count(), 2)
-        self.assertEqual(Book.objects.get(id=response.data['id']).title, 'Test Book')
+        self.assertEqual(Book.objects.get(id=response.data["id"]).title, "Test Book")
 
     def test_get_books(self):
-        response = self.client.get(self.url, format='json')
+        response = self.client.get(self.url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['title'], self.book.title)
+        self.assertEqual(response.data[0]["title"], self.book.title)
 
     def test_get_book_detail(self):
-        url = reverse('book-detail', args=[self.book.id])
-        response = self.client.get(url, format='json')
+        url = reverse("book-detail", args=[self.book.id])
+        response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], self.book.title)
+        self.assertEqual(response.data["title"], self.book.title)
 
     def test_update_book(self):
-        url = reverse('book-detail', args=[self.book.id])
+        url = reverse("book-detail", args=[self.book.id])
         updated_data = self.valid_data.copy()
-        updated_data['title'] = 'Updated Title'
-        response = self.client.put(url, updated_data, format='json')
+        updated_data["title"] = "Updated Title"
+        response = self.client.put(url, updated_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Book.objects.get(id=self.book.id).title, 'Updated Title')
+        self.assertEqual(Book.objects.get(id=self.book.id).title, "Updated Title")
 
     def test_delete_book(self):
-        url = reverse('book-detail', args=[self.book.id])
-        response = self.client.delete(url, format='json')
+        url = reverse("book-detail", args=[self.book.id])
+        response = self.client.delete(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Book.objects.count(), 0)
